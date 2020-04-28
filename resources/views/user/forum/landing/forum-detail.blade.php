fd36cbb728cac5889445d79dfc2863a2895e7a24
DESKTOP-V2N80Q <gendoh75@gmail.com>
2020-04-28 14:38:44 +0700

    update

--- resources/views/user/forum/landing/forum-detail.blade.php
+++ resources/views/user/forum/landing/forum-detail.blade.php
@@ -40,7 +40,6 @@ Detail
 	<div class="container hy-page-con hy-page-pos">
 		<div class="row">
 			<div class="col s12 m12 l8">
-
 				<div class="row">
 
 					<div class="col s12 m12 l12">
@@ -142,155 +141,10 @@ Detail
 					</div>
 
 				</div>
-
 			</div>
 
 			<div class="col s12 m12 l4 rightbar-post-hy">
-
-				<div class="row">
-					<div class="col s12 m12 l12">
-						<div class="hy-title-list">
-							<h5>Post Rekomendasi</h5>
-						</div>
-
-						<ul class="collection list-post">
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-						</ul>
-					</div>
-
-					<div class="col s12 m12 l12">
-						<div class="hy-title-list">
-							<h5>Tag Rekomendasi</h5>
-						</div>
-
-						<div class="card card-hy-f" style="border:0">
-							<div class="card-content">
-
-								<a href="#!" class="chip-hy">
-									Jane
-								</a>
-
-								<a href="#!" class="chip-hy">
-									Jane
-								</a>
-
-								<a href="#!" class="chip-hy">
-									Jane Doe
-								</a>
-
-								<a href="#!" class="chip-hy">
-									Jane
-								</a>
-
-								<a href="#!" class="chip-hy">
-									Jane
-								</a>
-
-							</div>
-						</div>
-
-					</div>
-
-				</div>
-
+				@include('user.app.right-post')
 			</div>
 		</div>
 	</div>
--- resources/views/user/forum/landing/forum-home.blade.php
+++ resources/views/user/forum/landing/forum-home.blade.php
@@ -41,18 +41,16 @@ Forum
 	<div class="container hy-page-con" style="width: 85%">
 		<div class="row">
 			<div class="col s12 m12 l8">
-				
 				<div class="hy-title-list row valign-wrapper-hy">
 					<div class="col s12 m12 l6">
 						<h5>Kategori</h5>
 					</div>
 					<div class="col s12 m12 l6 valign right-align">
-							<a href="{{route('UserPostAddNewPost')}}" class='waves-effect btn-flat hy-btn-flat-2'><i class="material-icons left">rate_review</i>Buat Post Baru Sekarang</a>
+						<a href="{{route('UserPostAddNewPost')}}" class='waves-effect btn-flat hy-btn-flat-2'><i class="material-icons left">rate_review</i>Buat Post Baru Sekarang</a>
 					</div>
 				</div>
 
 				<ul class="collection" style="border:0">
-
 					<li class="collection-item">
 						<div class="row valign-wrapper-hy" style="margin-bottom: 0">
 							<div class="col s12 m12 l6 valign">
@@ -142,150 +140,11 @@ Forum
 							</div>
 						</div>
 					</li>
-
 				</ul>
-
 			</div>
 
-			<div class="col s12 m12 l4 rightbar-post-hy">
-				<div class="hy-title-list">
-					<h5>Rekomendasi Post</h5>
-				</div>
-
-				<div class="row">
-					<div class="col s12 m12 l12">
-						<ul class="collection list-post">
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-							<li class="collection-item">
-								<a href="#!" class="tooltipped" data-position="top" data-tooltip="7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur">
-									<div class="card card-hy-f">
-										<div class="card-content">
-
-											<div class="row valign-wrapper-hy">
-												<div class="col s12 m12 l4 valign">
-													<img src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1579790778/ljwbwwpqqqzv3au5vsnn.jpg">
-												</div>
-
-												<div class="col s12 m12 l8">
-													<h6><b>7 Rekomendasi Film Bertema Wabah Virus Untuk Menghibur</b></h6>
-													<ul class="collection colection-hy-f">
-														<li class="collection-item colect-hy-f">
-															Kebun
-														</li>
-													</ul>
-												</div>
-											</div>
-
-										</div>
-									</div>
-								</a>
-							</li>
-
-						</ul>
-					</div>
-				</div>
-				
-
+			<div class="col s12 m12 l4 rightbar-post-hy">		
+				@include('user.app.right-post')
 			</div>
 		</div>
 	</div>
