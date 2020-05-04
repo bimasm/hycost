1d21ad24e2a923b2753457319a8f81deab54afc1
DESKTOP-V2N80Q <gendoh75@gmail.com>
2020-05-05 04:59:33 +0700

    tiny

--- resources/views/user/forum/dashboard/user-add-newpost-forum.blade.php
+++ resources/views/user/forum/dashboard/user-add-newpost-forum.blade.php
@@ -174,64 +174,14 @@ Dashboard Forum User
 
 	tinymce.init({
 		selector: 'textarea#full-featured-non-premium',
-
-		plugins: 'preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists imagetools textpattern charmap quickbars emoticons',
-
-		imagetools_cors_hosts: ['picsum.photos'],
-
+		height: 500,
 		menubar: false,
 
-		toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | | charmap emoticons | preview | insertfile image media link codesample | ltr rtl',
+		plugins: 'preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen link template codesample charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textpattern charmap emoticons',
 
-		toolbar_sticky: true,
-		autosave_ask_before_unload: true,
-		autosave_interval: "30s",
-		autosave_prefix: "{path}{query}-{id}-",
-		autosave_restore_when_empty: false,
-		autosave_retention: "2m",
-		image_advtab: true,
+		toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | | charmap emoticons | preview | link',
+
 		content_css: '{{asset('asset/css/tyni.css')}}',
-		link_list: [
-		{ title: 'My page 1', value: 'http://www.tinymce.com' },
-		{ title: 'My page 2', value: 'http://www.moxiecode.com' }
-		],
-		image_list: [
-		{ title: 'My page 1', value: 'http://www.tinymce.com' },
-		{ title: 'My page 2', value: 'http://www.moxiecode.com' }
-		],
-		image_class_list: [
-		{ title: 'None', value: '' },
-		{ title: 'Some class', value: 'class-name' }
-		],
-		importcss_append: true,
-		height: 400,
-		file_picker_callback: function (callback, value, meta) {
-			/* Provide file and text for the link dialog */
-			if (meta.filetype === 'file') {
-				callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
-			}
-
-			/* Provide image and alt text for the image dialog */
-			if (meta.filetype === 'image') {
-				callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
-			}
-
-			/* Provide alternative source and posted for the media dialog */
-			if (meta.filetype === 'media') {
-				callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
-			}
-		},
-		templates: [
-		{ title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
-		{ title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
-		],
-		template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-		template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-		height: 500,
-		image_caption: true,
-		quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage',
-		toolbar_mode: 'sliding',
-		contextmenu: "link image imagetools",
 	});
 
 </script>
